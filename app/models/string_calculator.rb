d63afb78dfa8c139a0e0530dbcd6d3d1e6c5d670
Ram Prabesh <ramprabeshchauhan43@gmail.com>
2025-04-17 21:56:46 +0530

    Add test and implementation for newline separators

--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -2,6 +2,6 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    numbers.split(",").map { |num| num.to_i }.sum
+    numbers.split(/,|\n/).map { |num| num.to_i }.sum
   end
 end
